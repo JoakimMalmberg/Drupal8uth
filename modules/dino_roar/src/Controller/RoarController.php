47b76ee79291b728733146f785d891bb0bcf9706
Joakim <jmalmberg.web@gmail.com>
2019-08-13 16:58:41 +0200

    Done

--- modules/dino_roar/src/Controller/RoarController.php
+++ modules/dino_roar/src/Controller/RoarController.php
@@ -1,8 +1,56 @@
 <?php
 
-namespace
+namespace Drupal\dino_roar\Controller;
 
-class RoarController
+use Drupal\Core\Controller\ControllerBase;
+use Drupal\Core\Logger\LoggerChannelFactoryInterface;
+use Drupal\dino_roar\Jurassic\RoarGenerator;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Symfony\Component\HttpFoundation\Response;
+
+
+class RoarController extends ControllerBase
 {
+  /**
+   * @var RoarGenerator
+   */
+  private $roarGenerator;
 
+  /**
+   * RoarController constructor.
+   * @param RoarGenerator $roarGenerator
+   * @param LoggerChannelFactoryInterface $loggerFactory
+   */
+  public function __construct(RoarGenerator $roarGenerator, LoggerChannelFactoryInterface $loggerFactory)
+  {
+    $this->roarGenerator = $roarGenerator;
+    $this->loggerFactory = $loggerFactory;
+  }
+
+  /**
+   * @param $count
+   * @return Response
+   */
+  public function roar($count)
+  {
+    $roar = $this->roarGenerator->getRoar($count);
+    $this->loggerFactory->get('default')
+      ->debug($roar);
+
+    return [
+      '#title' => $roar
+    ];
+  }
+
+  /**
+   * @param ContainerInterface $container
+   * @return ControllerBase|RoarController
+   */
+  public static function create(ContainerInterface $container)
+  {
+    $roarGenerator = $container->get('dino_roar.roar_generator');
+    $loggerFactory = $container->get('logger.factory');
+
+    return new static($roarGenerator, $loggerFactory);
+  }
 }
---
+++ modules/dino_roar/src/Jurassic/DinoListener.php
@@ -0,0 +1,44 @@
+<?php
+
+
+namespace Drupal\dino_roar\Jurassic;
+
+
+use Drupal\Core\Logger\LoggerChannelFactoryInterface;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpKernel\Event\GetResponseEvent;
+use Symfony\Component\HttpKernel\KernelEvents;
+
+class DinoListener implements EventSubscriberInterface
+{
+
+  /**
+   * @var LoggerChannelFactoryInterface
+   */
+  private $loggerFactory;
+
+  public function __construct(LoggerChannelFactoryInterface $loggerFactory)
+  {
+
+    $this->loggerFactory = $loggerFactory;
+  }
+
+  public function onKernelRequest(GetResponseEvent $event)
+  {
+    $request = $event->getRequest();
+    $shouldRoar = $request->query->get('roar');
+
+    if ($shouldRoar) {
+      $this->loggerFactory->get('default')
+        ->debug('Roar requested ROOOOOOOOAR');
+    }
+  }
+
+  public static function getSubscribedEvents()
+  {
+    return [
+      KernelEvents::REQUEST => 'onKernelRequest'
+    ];
+  }
+
+}
